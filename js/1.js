e6564cf2f109baf405f353c2fe023353b6199ae2
nej92 <emanunez26@gmail.com>
2021-08-21 22:39:56 -0300

    numFinish

--- js/1.js
+++ js/1.js
@@ -1,96 +1,87 @@
-let nro1 = document.getElementById('1');
-let juegobtns1 = document.getElementsByClassName('juego1');
-let valorbtn1 = document.getElementById('valor1');
-let valorbtn2 = document.getElementById('valor2');
-let valorbtn3 = document.getElementById('valor3');
-let valorbtn4 = document.getElementById('valor4');
-let valorbtn5 = document.getElementById('valor5');
-let valorbtn6 = document.getElementById('valor6');
+//JUGADOR 1
+let totalJugador1 = document.getElementById('TJ1');
+let sumattJ1 = document.getElementById('sumarTotalJug1');
 
-//CAMBIAR MUESTRA DE BOTONES
-function calc1 () {
-    nro1.style="display: none;";
-    let i;
-    for(i=0; i < juegobtns1.length; i++){
-        juegobtns1[i].style = "display: block;";
-    }
+function sumarJ1 (){
+    sumattJ1.style="display: none;";
+    let ttJ1 = document.createElement('p');
+    let unoJ1 = document.getElementById('1Jugador1').value;
+    let dosJ1 = document.getElementById('2Jugador1').value;
+    let tresJ1 = document.getElementById('3Jugador1').value;
+    let cuatroJ1 = document.getElementById('4Jugador1').value;
+    let cincoJ1 = document.getElementById('5Jugador1').value;
+    let seisJ1 = document.getElementById('6Jugador1').value;
+    let escJ1 = document.getElementById('escJugador1').value;
+    let fullJ1 = document.getElementById('fullJugador1').value;
+    let pokerJ1 = document.getElementById('pokerJugador1').value;
+    let genJ1 = document.getElementById('genJugador1').value;
+    let dgenJ1 = document.getElementById('dgenJugador1').value;
+    ttJ1.innerHTML  = parseInt(unoJ1) + parseInt(dosJ1) + parseInt(tresJ1) + parseInt(cuatroJ1) + parseInt(cincoJ1) + parseInt(seisJ1) + parseInt(escJ1) + parseInt(fullJ1) + parseInt(pokerJ1) + parseInt(genJ1) + parseInt(dgenJ1);
+    totalJugador1.appendChild(ttJ1);
 }
 
-//MUESTRA DE VALORES
-valorbtn1.addEventListener('click', agregar1alDOM);
-  
-    function agregar1alDOM (){ 
-        for(i=0; i < juegobtns1.length; i++){
-            juegobtns1[i].style = "display: none";
-        }
-        let valorDefjuego1 = document.getElementById('valorDefjuego1');
-        let valorjuego1 = document.createElement('h6');
-        valorjuego1.innerHTML = 1;
-        valorDefjuego1.appendChild(valorjuego1);
-  
+//JUGADOR 2
+let totalJugador2 = document.getElementById('TJ2');
+let sumattJ2 = document.getElementById('sumarTotalJug2');
+
+function sumarJ2 (){
+    sumattJ2.style="display: none;";
+    let ttJ2 = document.createElement('p');
+    let unoJ2 = document.getElementById('1Jugador2').value;
+    let dosJ2 = document.getElementById('2Jugador2').value;
+    let tresJ2 = document.getElementById('3Jugador2').value;
+    let cuatroJ2 = document.getElementById('4Jugador2').value;
+    let cincoJ2 = document.getElementById('5Jugador2').value;
+    let seisJ2 = document.getElementById('6Jugador2').value;
+    let escJ2 = document.getElementById('escJugador2').value;
+    let fullJ2 = document.getElementById('fullJugador2').value;
+    let pokerJ2 = document.getElementById('pokerJugador2').value;
+    let genJ2 = document.getElementById('genJugador2').value;
+    let dgenJ2 = document.getElementById('dgenJugador2').value;
+    ttJ2.innerHTML  = parseInt(unoJ2) + parseInt(dosJ2) + parseInt(tresJ2) + parseInt(cuatroJ2) + parseInt(cincoJ2) + parseInt(seisJ2) + parseInt(escJ2) + parseInt(fullJ2) + parseInt(pokerJ2) + parseInt(genJ2) + parseInt(dgenJ2);
+    totalJugador2.appendChild(ttJ2);
 }
 
-valorbtn2.addEventListener('click', agregar2alDOM);
-  
-    function agregar2alDOM (){ 
-        for(i=0; i < juegobtns1.length; i++){
-            juegobtns1[i].style = "display: none";
-        }
-        let valorDefjuego1 = document.getElementById('valorDefjuego1');
-        let valorjuego1 = document.createElement('h6');
-        valorjuego1.innerHTML = 2;
-        valorDefjuego1.appendChild(valorjuego1);
-  
+//JUGADOR 3
+let totalJugador3 = document.getElementById('TJ3');
+let sumattJ3 = document.getElementById('sumarTotalJug3');
+
+function sumarJ3 (){
+    sumattJ3.style="display: none;";
+    let ttJ3 = document.createElement('p');
+    let unoJ3 = document.getElementById('1Jugador3').value;
+    let dosJ3 = document.getElementById('2Jugador3').value;
+    let tresJ3 = document.getElementById('3Jugador3').value;
+    let cuatroJ3 = document.getElementById('4Jugador3').value;
+    let cincoJ3 = document.getElementById('5Jugador3').value;
+    let seisJ3 = document.getElementById('6Jugador3').value;
+    let escJ3 = document.getElementById('escJugador3').value;
+    let fullJ3 = document.getElementById('fullJugador3').value;
+    let pokerJ3= document.getElementById('pokerJugador3').value;
+    let genJ3 = document.getElementById('genJugador3').value;
+    let dgenJ3 = document.getElementById('dgenJugador3').value;
+    ttJ3.innerHTML  = parseInt(unoJ3) + parseInt(dosJ3) + parseInt(tresJ3) + parseInt(cuatroJ3) + parseInt(cincoJ3) + parseInt(seisJ3) + parseInt(escJ3) + parseInt(fullJ3) + parseInt(pokerJ3) + parseInt(genJ3) + parseInt(dgenJ3);
+    totalJugador3.appendChild(ttJ3);
 }
 
-valorbtn3.addEventListener('click', agregar3alDOM);
-  
-    function agregar3alDOM (){ 
-        for(i=0; i < juegobtns1.length; i++){
-            juegobtns1[i].style = "display: none";
-        }
-        let valorDefjuego1 = document.getElementById('valorDefjuego1');
-        let valorjuego1 = document.createElement('h6');
-        valorjuego1.innerHTML = 3;
-        valorDefjuego1.appendChild(valorjuego1);
-  
-    }
+//JUGADOR 4
+let totalJugador4 = document.getElementById('TJ4');
+let sumattJ4 = document.getElementById('sumarTotalJug4');
 
-valorbtn4.addEventListener('click', agregar4alDOM);
-  
-    function agregar4alDOM (){ 
-        for(i=0; i < juegobtns1.length; i++){
-            juegobtns1[i].style = "display: none";
-        }
-        let valorDefjuego1 = document.getElementById('valorDefjuego1');
-        let valorjuego1 = document.createElement('h6');
-        valorjuego1.innerHTML = 4;
-        valorDefjuego1.appendChild(valorjuego1);
-  
-    }
-
-valorbtn5.addEventListener('click', agregar5alDOM);
-  
-    function agregar5alDOM (){ 
-        for(i=0; i < juegobtns1.length; i++){
-            juegobtns1[i].style = "display: none";
-        }
-        let valorDefjuego1 = document.getElementById('valorDefjuego1');
-        let valorjuego1 = document.createElement('h6');
-        valorjuego1.innerHTML = 5;
-        valorDefjuego1.appendChild(valorjuego1);
-  
-    }
-
-valorbtn6.addEventListener('click', agregar6alDOM);
-  
-    function agregar6alDOM (){ 
-        for(i=0; i < juegobtns1.length; i++){
-            juegobtns1[i].style = "display: none";
-        }
-        let valorDefjuego1 = document.getElementById('valorDefjuego1');
-        let valorjuego1 = document.createElement('h6');
-        valorjuego1.innerHTML = 6;
-        valorDefjuego1.appendChild(valorjuego1);
-  
-    }
+function sumarJ4 (){
+    sumattJ4.style="display: none;";
+    let ttJ4 = document.createElement('p');
+    let unoJ4 = document.getElementById('1Jugador4').value;
+    let dosJ4 = document.getElementById('2Jugador4').value;
+    let tresJ4 = document.getElementById('3Jugador4').value;
+    let cuatroJ4 = document.getElementById('4Jugador4').value;
+    let cincoJ4 = document.getElementById('5Jugador4').value;
+    let seisJ4 = document.getElementById('6Jugador4').value;
+    let escJ4 = document.getElementById('escJugador4').value;
+    let fullJ4 = document.getElementById('fullJugador4').value;
+    let pokerJ4 = document.getElementById('pokerJugador4').value;
+    let genJ4 = document.getElementById('genJugador4').value;
+    let dgenJ4 = document.getElementById('dgenJugador4').value;
+    ttJ4.innerHTML  = parseInt(unoJ4) + parseInt(dosJ4) + parseInt(tresJ4) + parseInt(cuatroJ4) + parseInt(cincoJ4) + parseInt(seisJ4) + parseInt(escJ4) + parseInt(fullJ4) + parseInt(pokerJ4) + parseInt(genJ4) + parseInt(dgenJ4);
+    totalJugador4.appendChild(ttJ4);
+}
